02f7a1237d0c057e1ddccc923a4d48376c9f1a0d
AlefMenez <107272586+AlefMenez@users.noreply.github.com>
2023-11-17 12:45:33 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flame/flame.dart';
+import 'package:flame/game.dart' hide Game;
+import 'package:flutter/material.dart';
+import 'package:fruit_ninja/src/game.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Flame.device.fullScreen();
+  Flame.device.setLandscape();
+  runApp(GameWidget(game: Game()));
+}
